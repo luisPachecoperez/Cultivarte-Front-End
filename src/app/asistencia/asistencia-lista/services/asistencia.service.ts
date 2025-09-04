3b7904263b565750966a9ef37a936aea773f6ce9
brayancig-crea <brayancig@gmail.com>
2025-09-04 18:13:16 -0500

    servicios en asistencias corregidos

--- src/app/asistencia/asistencia-lista/services/asistencia.service.ts
+++ src/app/asistencia/asistencia-lista/services/asistencia.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, input } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { delay, map } from 'rxjs/operators';
@@ -25,6 +25,7 @@ mutation UpdateAsistencias($input: UpdateAsistenciaInput!) {
       id_sede
       numero_asistentes
       foto
+      descripcion
       imagen
 
       sedes {
@@ -40,13 +41,14 @@ mutation UpdateAsistencias($input: UpdateAsistenciaInput!) {
 
       asistentes_sesiones {
         id_persona
+        eliminar
       }
     }
   }
 `;
 private readonly ACTUALIZAR_SESION = `
-mutation ActualizarSesion($input: UpdateSesionInput!) {
-  updateSesion(input: $input) {
+mutation updateAsistencias($input: UpdateSesionInput!) {
+  updateAsistencias(input: $input) {
     exitoso
     mensaje
   }
@@ -67,42 +69,6 @@ mutation ActualizarSesion($input: UpdateSesionInput!) {
     }).pipe(
       map(res => res.data.getPreAsistencia)
     );
-    // if (this.usarMock) {
-    //   console.log('📥 [MOCK] obteniendo detalle de asistencia para:', id_actividad);
-
-    //   const mockResponse = {
-    //     id_actividad: "550e8400-e29b446655440000",
-    //     id_sesion: "550e-e29b4466554400",
-    //     id_sede: "550e8400-e29b-41d4-a716-446655440000",
-    //     numero_asistentes: 0,
-    //     foto: "N", // 👈 Cambia a "N" para probar asistencia normal
-    //     imagen: "https://storage.googleapis.com/[BUCKET_NAME]/[IMAGE_NAME]",
-    //     sedes: [
-    //       { id_sede: "550e8400-e29b-41d4-a716-446655440001", nombre_sede: "Sede Central" },
-    //       { id_sede: "550e8400-e29b-41d4-a716-446655440002", nombre_sede: "Sede Norte" }
-    //     ],
-    //     beneficiarios: [
-    //       {
-    //         id_persona: "123e4567-e89b-12d3-a456-426614174000",
-    //         nombre_completo: "Juan Pérez",
-    //         id_sede: "550e8400-e29b-41d4-a716-446655440001"
-    //       },
-    //       {
-    //         id_persona: "123e4567-e89b-12d3-a456-426614174001",
-    //         nombre_completo: "María López",
-    //         id_sede: "550e8400-e29b-41d4-a716-446655440002"
-    //       }
-    //     ],
-    //     asistentes_sesiones: [
-    //       { id_persona: "123e4567-e89b-12d3-a456-426614174002" },
-    //       { id_persona: "123e4567-e89b-12d3-a456-426614174003" }
-    //     ]
-    //   };
-
-    //   return of(mockResponse).pipe(delay(800));
-    // }
-
-    // return this.http.post<any>(`${this.apiUrl}/detalle`, { id_actividad });
   }
 
   // 🔹 Guardar asistencia (unificado)
@@ -116,23 +82,16 @@ mutation ActualizarSesion($input: UpdateSesionInput!) {
   }
 
   // 🔹 Guardar asistencia fotográfica
-  guardarAsistenciaFotografica(asistencia: any):  Observable<{ exitoso: string; mensaje: string }>  {
-    const input = {
-      id_sesion: asistencia.id_sesion,
-      id_actividad: asistencia.id_actividad,
-      fecha_actividad: asistencia.fecha_actividad,
-      hora_inicio: asistencia.hora_inicio,
-      hora_fin: asistencia.hora_fin,
-      imagen: asistencia.imagen,
-      nro_asistentes: asistencia.nro_asistentes,
-    };
+  guardarAsistenciaFotografica(input: any):  Observable<{ exitoso: string; mensaje: string }>  {
+
+    console.log('📤 Enviando asistencia EN EL SERVICES:', input);
 
 
     return this.http.post<any>(this.apiUrl, {
-      query: this.ACTUALIZAR_SESION,
+      query: this.UPDATE_ASISTENCIAS,
       variables: { input }
     }).pipe(
-      map(res => res.data.updateSesion)
+      map(res => res.data.updateAsistencias)
     );
   }
 
